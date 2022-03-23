a0cc30bc02567ba06fb7aee8626eb3976ce96229
mihaelacepraga <mihaelacepraga@gmail.com>
2022-03-23 13:47:12 +0200

    test-case inputName

--- testCases-epasapoarte.ro/README.md
+++ testCases-epasapoarte.ro/README.md
@@ -1,7 +1,7 @@
 
 # epasapoarte.ro TestCases
     
-## 1. Access home page
+## 1. Access home page [code](https://github.com/mihaelacepraga/test-automation/blob/ePasapoarte/testCases-epasapoarte.ro/test/specs/homePage.e2e.js)
 #### Description:
      Open home page and check if it has correct title.
 #### Steps to reproduce:
@@ -13,8 +13,24 @@
 #### Test Data: 
      https://www.epasapoarte.ro/ 
 #### Status:
-    New
+    Done
 
 ## 2. Test Input Name
 #### Description:
-     Check if the page title is coreect. 
+      Open home page and check if input "Nume" are existing and it working properly.
+#### Steps to reproduce:
+    1. Go to https://www.epasapoarte.ro/;
+    2. Check if input "Nume" are existing and click on it;
+    3. Verify if appear validation message "Este obligatoriu.",when the input "Nume" lost focus;
+    4. Verify if appear validation message "Minim 2 caractere.", when add just a letter;
+    5. Verify if appear validation message "Minim 2 caractere.Numai litere.", when add an  other character than letters;
+    6. Verify if appear validation message "Numai litere.", when add in input name any others characters than letters;
+    7. Check if input value is "Cepraga";
+#### Expected result:
+     User should be able to access https://www.epasapoarte.ro/, 
+     to find input "Nume" and add value "Cepraga".
+#### Test Data: 
+     https://www.epasapoarte.ro/;
+     input value"Cepraga";
+#### Status:
+    Done
---
+++ testCases-epasapoarte.ro/test/specs/inputName.e2e.js
@@ -0,0 +1,56 @@
+describe('Open home page and add an input value', () => {
+
+    let inputName;
+
+    it('Should have the correct page title', async() => {
+        await browser.url('https://www.epasapoarte.ro/');
+        await expect(browser).toHaveTitle('Programari online pasapoarte');
+    });
+    
+    it('Should detect if input "Nume" are existing', async() => {
+        inputName = await $('[name="Nume"]')
+        await expect(inputName).toBeExisting()
+    });
+
+    it('Should have validation message "Este obligatoriu.",when the input "Nume" lost focus.',
+       async() => {
+        inputName.click()
+        let sectionHome = await $('#programare')
+        sectionHome.click()
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small')
+        await expect(validationMessage).toBeDisplayed()
+        await expect(validationMessage).toHaveText('Este obligatoriu.')
+        
+    });
+   
+    it('Should have validation message "Minim 2 caractere.", when add just a letter', async() => {
+        await inputName.setValue('m')
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small')
+        await expect(validationMessage).toBeDisplayed()
+        await expect(validationMessage).toHaveText('Minim 2 caractere.')
+        await inputName.clearValue()
+    });
+
+    it('Should have validation message "Minim 2 caractere.Numai litere.", when add an other character than letters', async() => {
+        await inputName.setValue('!')
+        let validationMessage = await $('.validation')
+        await expect(validationMessage).toBeDisplayed()
+        await expect(validationMessage).toHaveText('Minim 2 caractere.Numai litere.')
+        await inputName.clearValue()
+    });
+
+    it('Should have validation message "Numai litere.", when add in input name any others characters than letters', async() => {
+        await inputName.setValue('Ma3')
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small')
+        await expect(validationMessage).toBeDisplayed()
+        await expect(validationMessage).toHaveText('Numai litere.')
+        await inputName.clearValue()
+    });
+
+    it('Should have input value "Cepraga" and ', async() => {
+        await inputName.setValue('Cepraga')
+        await expect(inputName).toHaveAttr('ng-reflect-model', 'Cepraga')
+      
+    });
+
+});
