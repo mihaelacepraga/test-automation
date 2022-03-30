a6f37a4b01fbebc386272c9d866e484bf69623ff
mihaelacepraga <mihaelacepraga@gmail.com>
2022-03-30 10:28:12 +0300

    Changes in title and let

--- testCases-epasapoarte.ro/test/specs/inputFirstName.e2e.js
+++ testCases-epasapoarte.ro/test/specs/inputFirstName.e2e.js
@@ -1,6 +1,6 @@
 describe('Set the value for first name', () => {
 
-    let inputLastName;
+    let inputFirstName;
 
     it('Should have the correct page title', async() => {
         await browser.url('https://www.epasapoarte.ro/');
@@ -8,49 +8,49 @@ describe('Set the value for first name', () => {
     });
     
     it('Should detect if input "Prenume" are existing', async() => {
-        inputLastName = await $('[name="Prenume"]')
-        await expect(inputLastName).toBeExisting()
+        inputFirstName = await $('[name="Prenume"]');
+        await expect(inputFirstName).toBeExisting();
     });
 
     it('Should have validation message "Este obligatoriu.",when the input "Prenume" lost focus.',
        async() => {
-        inputLastName.click()
-        let sectionHome = await $('#programare')
-        sectionHome.click()
-        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[3]/small')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Este obligatoriu.')
+        inputFirstName.click();
+        let sectionHome = await $('#programare');
+        sectionHome.click();
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[3]/small');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Este obligatoriu.');
         
     });
    
     it('Should have validation message "Minim 2 caractere.", when add just a letter', async() => {
-        await inputLastName.setValue('R')
-        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[3]')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Minim 2 caractere.')
-        await inputLastName.clearValue()
+        await inputFirstName.setValue('R');
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[3]');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Minim 2 caractere.');
+        await inputFirstName.clearValue();
     });
 
     it('Should have validation message "Minim 2 caractere.Numai litere.", when add an other character than letters', async() => {
-        await inputLastName.setValue('=')
-        let validationMessage = await $('.validation')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Minim 2 caractere.Numai litere.')
-        await inputLastName.clearValue()
+        await inputFirstName.setValue('=');
+        let validationMessage = await $('.validation');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Minim 2 caractere.Numai litere.');
+        await inputFirstName.clearValue();
     });
 
     it('Should have validation message "Numai litere.", when add in input name any others characters than letters', async() => {
-        await inputLastName.setValue('Ramo5')
-        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[3]/small')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Numai litere.')
-        await inputLastName.clearValue()
+        await inputFirstName.setValue('Ramo5');
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[3]/small');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Numai litere.');
+        await inputFirstName.clearValue();
     });
 
     it('Should have input value "Andreea"', async() => {
-        await inputLastName.setValue('Andreea')
-        await expect(inputLastName).toHaveAttr('ng-reflect-model','Andreea')
-        await browser.pause(6000)
+        await inputFirstName.setValue('Andreea');
+        await expect(inputFirstName).toHaveValue('Andreea');
+        
     }); 
 
 });
