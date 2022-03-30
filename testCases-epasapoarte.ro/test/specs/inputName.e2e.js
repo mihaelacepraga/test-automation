ef664ea1bfb6965ed948d4ef04813500e3aa5e21
mihaelacepraga <mihaelacepraga@gmail.com>
2022-03-30 10:53:12 +0300

    Add the punctuation mark (;)

--- testCases-epasapoarte.ro/test/specs/inputName.e2e.js
+++ testCases-epasapoarte.ro/test/specs/inputName.e2e.js
@@ -1,4 +1,4 @@
-describe('Set an input value for last name', () => {
+describe('Set an input value for name', () => {
 
     let inputName;
 
@@ -8,49 +8,48 @@ describe('Set an input value for last name', () => {
     });
     
     it('Should detect if input "Nume" are existing', async() => {
-        inputName = await $('[name="Nume"]')
-        await expect(inputName).toBeExisting()
+        inputName = await $('[name="Nume"]');
+        await expect(inputName).toBeExisting();
     });
 
     it('Should have validation message "Este obligatoriu.",when the input "Nume" lost focus.',
        async() => {
-        inputName.click()
-        let sectionHome = await $('#programare')
-        sectionHome.click()
-        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Este obligatoriu.')
+        inputName.click();
+        let sectionHome = await $('#programare');
+        sectionHome.click();
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Este obligatoriu.');
         
     });
    
     it('Should have validation message "Minim 2 caractere.", when add just a letter', async() => {
-        await inputName.setValue('m')
-        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Minim 2 caractere.')
-        await inputName.clearValue()
+        await inputName.setValue('m');
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Minim 2 caractere.');
+        await inputName.clearValue();
     });
 
     it('Should have validation message "Minim 2 caractere.Numai litere.", when add an other character than letters', async() => {
-        await inputName.setValue('!')
-        let validationMessage = await $('.validation')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Minim 2 caractere.Numai litere.')
-        await inputName.clearValue()
+        await inputName.setValue('!');
+        let validationMessage = await $('.validation');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Minim 2 caractere.Numai litere.');
+        await inputName.clearValue();
     });
 
     it('Should have validation message "Numai litere.", when add in input name any others characters than letters', async() => {
-        await inputName.setValue('Ma3')
-        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small')
-        await expect(validationMessage).toBeDisplayed()
-        await expect(validationMessage).toHaveText('Numai litere.')
-        await inputName.clearValue()
+        await inputName.setValue('Ma3');
+        let validationMessage = await $('//*[@id="forma"]/div/div[1]/div[1]/div[1]/div[2]/small');
+        await expect(validationMessage).toBeDisplayed();
+        await expect(validationMessage).toHaveText('Numai litere.');
+        await inputName.clearValue();
     });
 
     it('Should have input value "Cepraga" and ', async() => {
-        await inputName.setValue('Cepraga')
-        await expect(inputName).toHaveAttr('ng-reflect-model', 'Cepraga')
-      
+        await inputName.setValue('Cepraga');
+        await expect(inputName).toHaveValue('Cepraga');
     });
 
 });
