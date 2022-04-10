6f4e251f01d5865a94c775707501d12700c52af1
mihaelacepraga <mihaelacepraga@gmail.com>
2022-04-10 21:43:35 +0300

    new test in inputPlace

--- testCases-epasapoarte.ro/test/specs/inputPlace.e2e.js
+++ testCases-epasapoarte.ro/test/specs/inputPlace.e2e.js
@@ -1,19 +1,30 @@
+var assert = require('assert');
+
 describe('Select the localitie', () => {
 
 let inputLoc;
 let chooseLoc;
+let twoLoc;
 let numberPers;
+let nrOfDays;
+let listDays;
+let avaibleMounth;
+const locationNumber = 44;
+const numberOfLeters = 15;
+const positionLeterI = 7;
+const appointmentHour = ' 15:30 ';
+
 
     it('Should have the correct page title', async() => {
         await browser.url('/');
         await expect(browser).toHaveTitle('Programari online pasapoarte');
+        await browser.maximizeWindow();
     });
 
     it('Should find the input localitie', async() => {
         inputLoc = await $('#inputloc').$('[role=button]');
-        inputLoc.click();
+        await inputLoc.click();
         await expect(inputLoc).toBeExisting();
-        browser.pause(4000);
     });
 
     it('Verify if is displayed "Alege locatia..."', async() => {
@@ -23,37 +34,37 @@ let numberPers;
 
     it('Verify if is displayed the list of localities', async() => {
         let displayTypeDoc = await $('#selectul').$$('option')[1];
-        displayTypeDoc.click();
+        await displayTypeDoc.click();
         await expect(displayTypeDoc).toHaveText('pasaport simplu electronic');
 
         let listLoc = await $('#collapseloc').$$('//*[@id="collapseloc"]/div/div/div/div');
         await expect( listLoc).toBeDisplayed();
-        await expect(listLoc).toHaveChildren(44);
+        await expect(listLoc).toHaveChildren(locationNumber);
     });
 
-    it('Verify if are existing a list of leters and can select city Iasi', async() => {
-        let displayTypeDoc = await $('#selectul').$$('option')[1];
-        await expect(displayTypeDoc).toHaveText('pasaport simplu electronic');
-        displayTypeDoc.click();
+    it('Verify if are existing a list of leters and can select "I"', async() => {
         let listLeters = await $('.litere').$$('tr>td');
-        await expect(listLeters).toBeElementsArrayOfSize(15);
-       let chooseLeter = listLeters[7].click();
+        await expect(listLeters).toBeElementsArrayOfSize(numberOfLeters);
+        let chooseLeter = await listLeters[positionLeterI].click();
         
     });
 
-    it('Check if there are 2 localities whith leter "I" and select "Iasi"', async() => {
-        let twoLoc = await $('.locatii').$$('.row .loctit');
+    it('Check if there are 2 localities whith leter "I"', async() => {
+
+        twoLoc = await $$('//*[@id="collapseloc"]/div/div/div/div/div');
         await expect(twoLoc).toBeElementsArrayOfSize(2);
-        let locIasi = await twoLoc[1];
-        await expect(locIasi).toHaveText('Iasi:');
-        let selectIasi = await $('//*[@id="collapseloc"]/div/div/div/div/div[2]/div[2]/button').click();
-        await browser.pause(3000);
+        
     });
 
-    it('Check if the city "Iasi" is selected', async() => {
-        chooseLoc = await $('#showlocatia');
+    it('Verify is selected city "Iasi"', async() => {
+
+        let locIasi = await twoLoc[1];
+        await expect(locIasi).toHaveTextContaining(' Bd. Primaverii, nr.36, Iasi');
+        let chooseIasi = await $('//*[@id="collapseloc"]/div/div/div/div/div[2]/div[2]/button');
+        chooseIasi.click();
         await expect(chooseLoc).toHaveText('Iasi');
     });
+    
      
     it('Check if the user can select one person', async () => {
         numberPers = await $$('#myTab');
@@ -63,11 +74,15 @@ let numberPers;
         await expect(selectNrPer).toBeEnabled();
     });
    
-    it('Verify if can select date', async () => {
-        let inputDate = await $('#showdata');
-        inputDate.click();
-        await expect(inputDate).toHaveText('Alege data si ora ...');
-        
-    });
-    
+    it('Verify how many mounth is displayed and choose mounth "Iulie"', async () => {
+
+         let inputDate = await $('#nav-tab').$$('a');
+         await expect(inputDate).toBeElementsArrayOfSize(inputDate.length); 
+
+         console.log("Count of mounth ",inputDate.length); 
+        let selectMounth = await inputDate[0];
+         await expect(selectMounth).toHaveAttr('id', 'iul-tab');
+
+        });
+
 });
